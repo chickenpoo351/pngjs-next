c254ec4bcba3b648d2c3745be5762da3066ddd05
chickenPoo <ivanemerald1209@gmail.com>
2026-08-01 22:14:24 -0400

    refactor format-normaliser.js this one was decently easy

--- src/bitmap/format-normaliser.js
+++ src/bitmap/format-normaliser.js
@@ -1,93 +1,80 @@
-"use strict";
-
 function dePalette(indata, outdata, width, height, palette) {
-  let pxPos = 0;
-  // use values from palette
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let color = palette[indata[pxPos]];
-
-      if (!color) {
-        throw new Error("index " + indata[pxPos] + " not in palette");
-      }
-
-      for (let i = 0; i < 4; i++) {
-        outdata[pxPos + i] = color[i];
-      }
-      pxPos += 4;
+  const byteLength = width * height * 4;
+  for (let pxPos = 0; pxPos < byteLength; pxPos += 4) {
+    const color = palette[indata[pxPos]];
+    if (!color) {
+      throw new Error(`index ${indata[pxPos]} not in palette`);
     }
+
+    outdata[pxPos] = color[0];
+    outdata[pxPos + 1] = color[1];
+    outdata[pxPos + 2] = color[2];
+    outdata[pxPos + 3] = color[3];
   }
 }
 
 function replaceTransparentColor(indata, outdata, width, height, transColor) {
-  let pxPos = 0;
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let makeTrans = false;
-
-      if (transColor.length === 1) {
-        if (transColor[0] === indata[pxPos]) {
-          makeTrans = true;
-        }
-      } else if (
-        transColor[0] === indata[pxPos]
-        && transColor[1] === indata[pxPos + 1]
-        && transColor[2] === indata[pxPos + 2]
-      ) {
+  const byteLength = width * height * 4;
+  for (let pxPos = 0; pxPos < byteLength; pxPos += 4) {
+    let makeTrans = false;
+    if (transColor.length === 1) {
+      if (transColor[0] === indata[pxPos]) {
         makeTrans = true;
       }
-      if (makeTrans) {
-        for (let i = 0; i < 4; i++) {
-          outdata[pxPos + i] = 0;
-        }
-      }
-      pxPos += 4;
+    } else if (
+      transColor[0] === indata[pxPos]
+      && transColor[1] === indata[pxPos + 1]
+      && transColor[2] === indata[pxPos + 2]
+    ) {
+      makeTrans = true;
+    }
+    if (makeTrans) {
+      outdata[pxPos] = 0;
+      outdata[pxPos + 1] = 0;
+      outdata[pxPos + 2] = 0;
+      outdata[pxPos + 3] = 0;
     }
   }
 }
 
 function scaleDepth(indata, outdata, width, height, depth) {
-  let maxOutSample = 255;
-  let maxInSample = Math.pow(2, depth) - 1;
-  let pxPos = 0;
+  const scale = 255 / (2 ** depth - 1);
+  const byteLength = width * height * 4;
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      for (let i = 0; i < 4; i++) {
-        outdata[pxPos + i] = Math.floor(
-          (indata[pxPos + i] * maxOutSample) / maxInSample + 0.5,
-        );
-      }
-      pxPos += 4;
+  for (let pxPos = 0; pxPos < byteLength; pxPos += 4) {
+    for (let i = 0; i < 4; i++) {
+      outdata[pxPos + i] = Math.floor(indata[pxPos + i] * scale + 0.5);
     }
   }
 }
 
 export function formatNormaliser(indata, imageData, skipRescale = false) {
-  let depth = imageData.depth;
-  let width = imageData.width;
-  let height = imageData.height;
-  let colorType = imageData.colorType;
-  let transColor = imageData.transColor;
-  let palette = imageData.palette;
+  const {
+    depth,
+    width,
+    height,
+    colorType,
+    transColor,
+    palette,
+  } = imageData;
 
   let outdata = indata; // only different for 16 bits
 
   if (colorType === 3) {
     // paletted
     dePalette(indata, outdata, width, height, palette);
-  } else {
-    if (transColor) {
-      replaceTransparentColor(indata, outdata, width, height, transColor);
+    return outdata;
+  }
+  if (transColor) {
+    replaceTransparentColor(indata, outdata, width, height, transColor);
+  }
+  // if it needs scaling
+  if (depth !== 8 && !skipRescale) {
+    // if we need to change the buffer size
+    if (depth === 16) {
+      outdata = new Uint8Array(width * height * 4);
     }
-    // if it needs scaling
-    if (depth !== 8 && !skipRescale) {
-      // if we need to change the buffer size
-      if (depth === 16) {
-        outdata = Buffer.alloc(width * height * 4);
-      }
-      scaleDepth(indata, outdata, width, height, depth);
-    }
+    scaleDepth(indata, outdata, width, height, depth);
   }
   return outdata;
 }
