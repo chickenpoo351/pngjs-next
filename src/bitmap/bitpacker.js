dceb4109096b8bd94fb29de37133f02742732721
chickenPoo <ivanemerald1209@gmail.com>
2026-08-01 21:50:53 -0400

    destructure options object in bitpacker.js

--- src/bitmap/bitpacker.js
+++ src/bitmap/bitpacker.js
@@ -1,6 +1,13 @@
 import constants from "../shared/constants.js";
 
 export function bitPacker(dataIn, width, height, options) {
+  const {
+    bitDepth,
+    colorType,
+    inputColorType,
+    inputHasAlpha,
+    bgColor,
+  } = options;
   function writeU16BE(out, offset, value) {
     out[offset] = value >>> 8;
     out[offset + 1] = value & 0xff;
@@ -13,11 +20,11 @@ export function bitPacker(dataIn, width, height, options) {
   const outHasAlpha = [
     constants.COLORTYPE_COLOR_ALPHA,
     constants.COLORTYPE_ALPHA,
-  ].includes(options.colorType);
+  ].includes(colorType);
 
   if (
-    options.colorType === options.inputColorType
-    && options.bitDepth === 8
+    colorType === inputColorType
+    && bitDepth === 8
   ) {
     return dataIn;
   }
@@ -25,12 +32,12 @@ export function bitPacker(dataIn, width, height, options) {
   const data = dataIn;
 
   let maxValue = 255;
-  let inBpp = constants.COLORTYPE_TO_BPP_MAP[options.inputColorType];
-  if (inBpp === 4 && !options.inputHasAlpha) {
+  let inBpp = constants.COLORTYPE_TO_BPP_MAP[inputColorType];
+  if (inBpp === 4 && !inputHasAlpha) {
     inBpp = 3;
   }
-  let outBpp = constants.COLORTYPE_TO_BPP_MAP[options.colorType];
-  if (options.bitDepth === 16) {
+  let outBpp = constants.COLORTYPE_TO_BPP_MAP[colorType];
+  if (bitDepth === 16) {
     maxValue = 65535;
     inBpp *= 2;
     outBpp *= 2;
@@ -40,7 +47,7 @@ export function bitPacker(dataIn, width, height, options) {
   let inIndex = 0;
   let outIndex = 0;
 
-  const bg = options.bgColor ?? {};
+  const bg = bgColor ?? {};
   const bgRed = bg.red ?? maxValue;
   const bgGreen = bg.green ?? maxValue;
   const bgBlue = bg.blue ?? maxValue;
@@ -51,9 +58,9 @@ export function bitPacker(dataIn, width, height, options) {
 
   function readRGBA() {
     alpha = maxValue;
-    switch (options.inputColorType) {
+    switch (inputColorType) {
       case constants.COLORTYPE_COLOR_ALPHA: {
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           red = data[inIndex];
           green = data[inIndex + 1];
           blue = data[inIndex + 2];
@@ -67,7 +74,7 @@ export function bitPacker(dataIn, width, height, options) {
         break;
       }
       case constants.COLORTYPE_COLOR: {
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           red = data[inIndex];
           green = data[inIndex + 1];
           blue = data[inIndex + 2];
@@ -79,7 +86,7 @@ export function bitPacker(dataIn, width, height, options) {
         break;
       }
       case constants.COLORTYPE_ALPHA: {
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           red = green = blue = data[inIndex];
           alpha = data[inIndex + 1];
         } else {
@@ -89,7 +96,7 @@ export function bitPacker(dataIn, width, height, options) {
         break;
       }
       case constants.COLORTYPE_GRAYSCALE: {
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           red = green = blue = data[inIndex];
         } else {
           red = green = blue = readU16BE(data, inIndex);
@@ -97,11 +104,11 @@ export function bitPacker(dataIn, width, height, options) {
         break;
       }
       default: {
-        throw new Error(`Unsupported input color type: ${options.inputColorType}`);
+        throw new Error(`Unsupported input color type: ${inputColorType}`);
       }
     }
 
-    if (options.inputHasAlpha && !outHasAlpha) {
+    if (inputHasAlpha && !outHasAlpha) {
       const a = alpha / maxValue;
       red = Math.round((1 - a) * bgRed + a * red);
       green = Math.round((1 - a) * bgGreen + a * green);
@@ -114,10 +121,10 @@ export function bitPacker(dataIn, width, height, options) {
   for (let i = 0; i < pixels; i++) {
     readRGBA();
 
-    switch (options.colorType) {
+    switch (colorType) {
       case constants.COLORTYPE_COLOR_ALPHA:
       case constants.COLORTYPE_COLOR:
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           outData[outIndex] = red;
           outData[outIndex + 1] = green;
           outData[outIndex + 2] = blue;
@@ -139,7 +146,7 @@ export function bitPacker(dataIn, width, height, options) {
       case constants.COLORTYPE_GRAYSCALE: {
         // Convert to grayscale and alpha
         const gray = Math.round((red + green + blue) / 3);
-        if (options.bitDepth === 8) {
+        if (bitDepth === 8) {
           outData[outIndex] = gray;
           if (outHasAlpha) {
             outData[outIndex + 1] = alpha;
@@ -153,7 +160,7 @@ export function bitPacker(dataIn, width, height, options) {
         break;
       }
       default:
-        throw new Error(`Unknown color type: ${options.colorType}`);
+        throw new Error(`Unknown color type: ${colorType}`);
     }
 
     inIndex += inBpp;
